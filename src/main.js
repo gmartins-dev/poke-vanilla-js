b56c1ec03538f73ebcc58ca02a3e23b0320fdfd2
gmartins-dev <guilhermemm.dev@gmail.com>
2026-03-14 18:08:14 -0300

    chore: setup tailwind v4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import "./style.css"
+
 document.querySelector("#app").innerHTML = `
-  <h1>Hello World</h1>
-  <p>Pokedex project starting point</p>
+  <main class="mx-auto mt-16 max-w-xl px-6 text-center">
+    <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <p class="mt-4 text-slate-600">Pokedex project starting point</p>
+  </main>
 `
